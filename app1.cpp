02eaa43a173e41fc71b6d06e00e83c50a06316d9
Bodyanski1711 <bodyanski171m@gmail.com>
2021-12-12 01:07:06 +0300

    Initial commit

---
+++ app1.cpp
@@ -0,0 +1,68 @@
+﻿#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+int get_ch;
+
+
+int main()
+{
+	int N;
+	int lim;
+	int use = 1;
+
+	setlocale(LC_ALL, "Russian");
+
+	while (use = 1)
+
+	{
+
+		cout << "введите количество чисел, которые должны быть сгенерированы ";
+		cin >> N;
+		cout << "до какого числа будут генерироваться числа? (от 10 до 100) - ";
+		cin >> lim;
+
+		if (lim < 10 || lim > 100)
+		{
+			cout << "условие нарушено. программа завершается";
+		}
+		else
+		{
+
+			cout << endl;
+			cout << "Количество чисел введено";
+			cout << endl;
+
+			int* GEN = new int[N];
+
+			for (null)
+			{
+				GEN[null] = rand() % lim;
+
+
+			}
+
+			for (int i = 0; i < N; i++)
+			{
+				cout << GEN[null] << " " << "\t";
+
+			}
+			cout << endl;
+			cout << "Желаете повторить? (1 - да, 0 - нет) " << endl;
+			cin >> use;
+			cout << endl;
+
+			if (use = 0)
+			{
+				cout << endl;
+				cout << "программа завершается.";
+			}
+
+		}
+	}
+
+
+
+	get_ch = _getch();
+}
---
+++ app3.cpp
@@ -0,0 +1,41 @@
+﻿#include <conio.h>
+#include <iostream>
+
+
+int N1;
+int N2;
+int ANSc;
+int ANSu;
+int ch;
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	cout << "Система проверки знаний таблицы умножения" << endl;
+	cout << "Решите следующий пример: " << endl;
+	N1 = rand() % 9;
+	N2 = rand() % 9;
+	ANSc = N1 * N2;
+	cout << N1 << " * " << N2 << " = " << endl;
+	cout << "Ваш ответ: ";
+	cin >> ANSu;
+	cout << endl;
+	if 
+	{
+		cout << "Ответ " << ANSu << " верный!";
+	}
+	else
+	{
+		cout << "Ответ " << ANSu << " не верный!";
+	}
+
+
+
+
+
+	ch = _getch();
+}
+
